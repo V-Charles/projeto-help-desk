c71018415f5371660f52d9da9585bd31c0d35802
Vinicius Charles <viniciuscharles3101@gmail.com>
2025-10-19 20:11:32 -0300

    feat(frontend): criar página index e arquivo CSS base

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>App Help Desk</title>
+    <link rel="stylesheet" href="global/style.css">
+</head>
+<body>
+    <header>
+        <img src="assets/images/logo.png" alt="Logo App Help Desk">
+        <h1>App Help Desk</h1>
+    </header>
+    <main>
+        <section class="card-login">
+            <h2>Login</h2>
+            <form action="">
+                <input type="email" name="" id="" placeholder="E-mail" required>
+                <input type="password" name="" id="" placeholder="Senha" required>
+                <button type="submit">Entrar</button>
+            </form>
+        </section>
+    </main>
+</body>
+</html>
