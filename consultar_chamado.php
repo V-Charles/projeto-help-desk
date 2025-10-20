77f54977838d760146dc1758d01b898fa35346c0
Vinicius Charles <viniciuscharles3101@gmail.com>
2025-10-19 21:23:11 -0300

    feat(frontend): criar página consultar chamado

---
+++ consultar_chamado.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>App Help Desk - Consulta de Chamado</title>
+    <link rel="stylesheet" href="global/style.css">
+</head>
+<body>
+    <header>
+        <img src="assets/images/logo.png" alt="Logo App Help Desk">
+        <h1>App Help Desk</h1>
+    </header>
+    <main>
+        <section class="card-consultar-chamado">
+            <div class="card">
+                <h2 class="card-header">Consulta de Chamado</h2>
+                <div class="card-body">
+                    <article class="chamado-card">
+                        <h3 class="chamado-title">Título do chamado</h3>
+                        <h4 class="chamado-category">Categoria</h4>
+                        <p class="chamado-description">Descrição do chamado</p>
+                    </article>
+                    <article class="chamado-card">
+                        <h3 class="chamado-title">Título do chamado</h3>
+                        <h4 class="chamado-category">Categoria</h4>
+                        <p class="chamado-description">Descrição do chamado</p>
+                    </article>
+                    <article class="chamado-card">
+                        <h3 class="chamado-title">Título do chamado</h3>
+                        <h4 class="chamado-category">Categoria</h4>
+                        <p class="chamado-description">Descrição do chamado</p>
+                    </article>
+                    <div class="form-actions">
+                        <button class="btn btn-warning" type="button">Voltar</button>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </main>
+</body>
+</html>
